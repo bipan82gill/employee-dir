49ef79c25d6699535751a121f0fa2af6ea6022ab
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-03-26 15:37:21 -0400

    adding header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import EmployeeHeader from './components/EmployeeHeader';
 import EmployeeCard from './components/EmployeeCard';
 
 function App() {
   return (
     <div className="App">
+      <EmployeeHeader />
       <EmployeeCard />
     
     </div>
---
+++ src/components/EmployeeHeader/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function EmployeeHeader(props){
+return(
+    <div className ="employeeList">
+        <h1>Employees List</h1>
+    </div>
+);
+}
+
+export default EmployeeHeader;
